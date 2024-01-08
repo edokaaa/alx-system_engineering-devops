88e9c27dc7144a9aa676ce25830af3366030fe8e
edoka <peter.edoka.242464@unn.edu.ng>
2024-01-08 15:53:20 +0100

    automate using puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,17 @@
+# A script that automate that task of creating a custom http header response
+# on nginx server
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run2':
+  command => '/usr/sbin/service nginx start',
+}
