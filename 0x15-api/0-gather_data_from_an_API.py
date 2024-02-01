35ffe0ad1aea32faef39dee46060673c813be7fc
edoka <peter.edoka.242464@unn.edu.ng>
2024-02-01 14:48:43 +0100

    put code inside a function

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,45 @@
 #!/usr/bin/python3
-"""A script that gathers data from an api."""
+"""Gather data from an API.
+
+A script that gathers data from an api.
+"""
 
 import requests
 import sys
 
-try:
-    user_id = int(sys.argv[1])
-except ValueError:
-    print("User ID must be integer")
-    exit(1)
 
-todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-    user_id)
-user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+def user_todos(user_id):
+    """Get the todos of a user."""
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        user_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-todo_list = requests.get(todo_url).json()
-user = requests.get(user_url).json()
+    todo_list = requests.get(todo_url).json()
+    user = requests.get(user_url).json()
 
-total_task = len(todo_list)
-completed = []
+    total_task = len(todo_list)
+    completed = []
 
-for todo in todo_list:
-    if todo['completed']:
-        completed.append(todo['title'])
-try:
-    username = user["name"]
-except KeyError as e:
-    print("User not found")
-    exit(1)
+    for todo in todo_list:
+        if todo['completed']:
+            completed.append(todo['title'])
+    try:
+        username = user["name"]
+    except KeyError as e:
+        print("User not found")
+        exit(1)
 
-print("Employee {} is done with tasks({}/{})".format(
-    user["name"], len(completed), total_task))
-for item in completed:
-    print("\t {}".format(item))
+    print("Employee {} is done with tasks({}/{})".format(
+        user["name"], len(completed), total_task))
+    for item in completed:
+        print("\t {}".format(item))
+
+
+if __name__ == "__main__":
+    try:
+        user_id = sys.argv[1]
+    except ValueError:
+        print("User ID must be integer")
+        exit(1)
+
+    user_todos(user_id)
