829f44409b908e664b6174d80dae32aa1cd7f670
edoka <peter.edoka.242464@unn.edu.ng>
2024-06-05 14:39:43 +0100

    subcribers from reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""How many subs?."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Query the Reddit API.
+
+    Args:
+        subreddit (str) - the id of the reddit user.
+    Returns:
+        The number of subcribers for the subreddit.
+    """
+    headers = {
+        "User-agent": "ubuntu:alxse:v1.0 (by /u/edokadev)"
+    }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers)
+    data = res.json().get('data')
+
+    if 'subscribers' in data:
+        return data.get('subscribers')
+    return 0
